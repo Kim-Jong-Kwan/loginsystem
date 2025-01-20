e28d39619177310582a6151081939878f17497d2
Kim-Jong-Kwan <kim71419678@gmail.com>
2025-01-20 09:12:10 +0900

    Update LoginSystemApplication.java

--- LoginSystem/src/main/java/com/itg/examp/LoginSystemApplication.java
+++ LoginSystem/src/main/java/com/itg/examp/LoginSystemApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LoginSystemApplication {
 
 	public static void main(String[] args) {
+		System.out.println("서버가 잘 실행되었습니다.");
 		SpringApplication.run(LoginSystemApplication.class, args);
 	}
 
