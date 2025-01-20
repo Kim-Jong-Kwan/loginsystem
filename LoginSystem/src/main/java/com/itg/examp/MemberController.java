2324f3a1be3e770868f98ae1cc327d71484f062a
itg <itg@DESKTOP-4A63QUS>
2025-01-20 09:28:51 +0900

    message

---
+++ LoginSystem/src/main/java/com/itg/examp/MemberController.java
@@ -0,0 +1,50 @@
+package com.itg.examp;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.itg.examp.dao.MemberDAO;
+import com.itg.examp.dto.MemberDTO;
+@RestController
+@RequestMapping("/member")
+public class MemberController {
+	@Autowired
+	MemberDAO dao;
+	@Autowired
+	Memberservice service;
+	@PostMapping("/signup")
+	public Map<String,Object> signupMember(MemberDTO member) {
+		//id는 4글자이상 
+		System.out.println(member.toString());
+		boolean ckid = service.checkId(member.getMid());
+		boolean ckpw = service.checkId(member.getMpw());
+		HashMap<String, Object> hm = new HashMap<>();
+		if(!ckid) {
+			hm.put("message","아이디를 점검하세요");
+		}
+		if(!ckpw) {
+			hm.put("message","비밀번호를 점검하세요");
+		}
+		int res = dao.signupMember(member);
+		if(res==1) {
+			hm.put("message", "회원가입성공");
+			hm.put("data", member);
+			System.out.println(member.getName()+"회원 가입성공");
+		}else {
+			hm.put("message", "회원가입실패 관리하세요");
+			System.out.println("회원가입실패");
+		}
+		return hm;
+	}
+	@PostMapping("/login")
+	public void signin() {}
+	@GetMapping("/logout")
+	public void signout() {}
+	@GetMapping("/listview")
+	public void listView() {}
+}
---
+++ LoginSystem/src/main/java/com/itg/examp/dao/MemberDAOImpl.java
@@ -0,0 +1,35 @@
+package com.itg.examp.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.itg.examp.dto.MemberDTO;
+@Repository
+public class MemberDAOImpl implements MemberDAO{
+	@Autowired
+	SqlSessionTemplate session;
+	@Override
+	public int signupMember(MemberDTO member) {
+		int res = session.insert("memberMapper.signup",member);
+		System.out.println("회원가입DAO:"+res);
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public MemberDTO signinMember(Map<String, String> data) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<MemberDTO> memberList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
